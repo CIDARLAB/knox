1d0809bde9b78318d89fc59731964dec62417631
evanbowman <ebowman@bu.edu>
2017-04-20 19:31:01 -0400

    re-implemented query page

--- src/main/resources/static/js/knox.js
+++ src/main/resources/static/js/knox.js
@@ -1,3 +1,21 @@
+
+// Wrapper JavaScript interface for the Knox API
+(function() {
+    const extensions = {
+        D3: "/designSpace/graph/d3"
+    };
+    
+    window.knox = {
+        // callback is of the form: function(err, jsonObj)
+        getGraph: function(id, callback) {
+            var query = "?targetSpaceID=" + encodeURIComponent(id);
+            d3.json(extensions.D3 + query, callback);
+        }
+    };
+})();
+
+// Utility for disabling scrolling.
+// Exposes the function disableScroll
 (function() {
     // left: 37, up: 38, right: 39, down: 40,
     // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
@@ -5,8 +23,9 @@
 
     function preventDefault(e) {
         e = e || window.event;
-        if (e.preventDefault)
+        if (e.preventDefault) {
             e.preventDefault();
+        }
         e.returnValue = false;  
     }
 
@@ -15,35 +34,120 @@
             preventDefault(e);
             return false;
         }
+        return true;
     }
 
     function disableScroll() {
-        if (window.addEventListener) // older FF
+        if (window.addEventListener) {
             window.addEventListener("DOMMouseScroll", preventDefault, false);
-        window.onwheel = preventDefault; // modern standard
-        window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-        window.ontouchmove  = preventDefault; // mobile
+        }
+        window.onwheel = preventDefault;
+        window.onmousewheel = document.onmousewheel = preventDefault;
+        window.ontouchmove  = preventDefault;
         document.onkeydown  = preventDefaultForScrollKeys;
     }
 
     window.disableScroll = disableScroll;
 })();
 
-window.onload = function() {
-    disableScroll();
-};
-
 (function($) {
     "use strict";
 
-    $(document).on("click", "a.page-scroll", function(event) {
-        var $anchor = $(this);
-        $("html, body").stop().animate({
-            scrollTop: ($($anchor.attr("href")).offset().top - 50)
-        }, 1250, "easeInOutExpo");
-        event.preventDefault();
+    function Target(id) {
+        this.layouts = {};
+        this.id = id;
+        $(id).width($(id).parent().width());
+        $(id).height($(id).parent().height());
+    }
+    
+    Target.prototype = {
+        appendGraph: function(id, graph) {
+            var force = (this.layouts[id] = d3.layout.force());
+            force.charge(-300).linkDistance(70);
+            force.nodes(graph.nodes).links(graph.links).size([
+                $(this.id).parent().width(), $(this.id).parent().height()
+            ]).start();
+            var svg = d3.select(this.id);
+            var link = svg.selectAll(".link")
+                .data(graph.links)
+                .enter().append("line")
+                .attr("class", "link");
+
+            var node = svg.selectAll(".node")
+                .data(graph.nodes)
+                .enter().append("circle")
+                .attr("class", "node")
+                .attr("r", 12)
+                .call(force.drag);
+
+            force.on("tick", function () {
+                link.attr("x1", function (d) {
+                    return d.source.x;
+                }).attr("y1", function (d) {
+                    return d.source.y;
+                }).attr("x2", function (d) {
+                    return d.target.x;
+                }).attr("y2", function (d) {
+                    return d.target.y;
+                });
+                node.attr("cx", function (d) {
+                    return d.x;
+                }).attr("cy", function (d) {
+                    return d.y;
+                });
+            });
+        },
+
+        removeGraph: function(id) {
+            delete this.layouts[id];
+        },
+
+        expandToFillParent() {
+            Object.keys(this.layouts).map((key, _) => {
+                var layout = this.layouts[key];
+                var width = $(this.id).parent().width();
+                var height = $(this.id).parent().height();
+                layout.size([width, height]);
+                layout.start();
+            });
+            $(this.id).width($(this.id).parent().width());
+            $(this.id).height($(this.id).parent().height());
+        }
+    };
+    
+    var targets = {
+        search: new Target("#search-svg")
+    };
+    
+    window.onload = function() {
+        disableScroll();  
+    };
+    
+    $("#search-tb").keydown(function(e) {
+        const submitKeyCode = 13;
+        if ((e.keyCode || e.which) == submitKeyCode) {
+            var graphName = this.value;
+            knox.getGraph(graphName, function(err, data) {
+                if (err) {
+                    window.alert(err);
+                } else {
+                    targets.search.appendGraph(graphName, data);
+                }
+            });
+        }
     });
 
+    window.onresize = function(e) {
+        var currentHash = window.location.hash.substr(1);
+        var currentSection = document.getElementById(currentHash);
+        if (currentSection) {
+            window.scrollTo(0, currentSection.offsetTop);
+        }
+        Object.keys(targets).map((key, _) => {
+            targets[key].expandToFillParent();
+        });
+    };
+    
     $("body").scrollspy({
         target: ".navbar-fixed-top",
         offset: 51
@@ -58,20 +162,4 @@ window.onload = function() {
             top: 100
         }
     });
-    
-    window.sr = ScrollReveal();
-    sr.reveal(".sr-icons", {
-        duration: 600,
-        scale: 0.3,
-        distance: "0px"
-    }, 200);
-    sr.reveal(".sr-button", {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal(".sr-contact", {
-        duration: 600,
-        scale: 0.3,
-        distance: "0px"
-    }, 300);
 })(jQuery);
