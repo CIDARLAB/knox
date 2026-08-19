fa5150a03cd6e765917fd88de1180a2a829d3c68
James Roberts <jmr@bu.edu>
2026-08-19 18:11:54 -0400

    Remove Unused Import

--- src/main/java/knox/spring/data/neo4j/config/RestClientConfig.java
+++ src/main/java/knox/spring/data/neo4j/config/RestClientConfig.java
@@ -1,7 +1,5 @@
 package knox.spring.data.neo4j.config;
 
-import java.time.Duration;
-
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
